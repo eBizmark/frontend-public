857baae640de0552e230a3e8a2adf9bfc6c9afb2
rezuki <rezky.wahyuda.tif18@polban.ac.id>
2021-05-17 15:45:43 +0800

    hiding navlink and add axios

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="w-full h-screen">
     <header class="bg-black invisible md:visible block fixed w-full z-40">
-      <nav class="flex justify-between w-full bg-purple-500 text-white p-4">
+      <nav class="flex justify-between w-full bg-white text-black p-4">
         <router-link to="/"><span class="font-semibold text-xl tracking-tight">E-Bizmark</span></router-link>
-        <div class="md:items-center md:w-auto flex">
-          <div class="md:flex hidden">
-            <a class="block md:text-white mr-4" href="/link">Link 1</a>
-            <a class="block md:text-white mr-4" href="/link">Link 2</a>
-            <a class="block md:text-white mr-4" href="/link">Link 3</a>
-            <a class="block md:text-white mr-4" href="/link">Link 4</a>
-          </div>
-          <div class="flex text-sm">
-            <a class="p-2 ml-2 bg-white text-teal-500 font-semibold leading-none border border-gray-100 rounded hover:border-transparent hover:bg-gray-100" href="/auth/signin">Login</a>
-            <a class="p-2 ml-2 bg-teal-500 text-gray-100 font-semibold leading-none border border-teal-600 rounded hover:border-transparent hover:bg-teal-600" href="/auth/signup">Sign up</a>
-          </div>
-        </div>
+<!--        <div class="md:items-center md:w-auto flex">-->
+<!--          <div class="md:flex hidden">-->
+<!--            <a class="block md:text-white mr-4" href="/link">Link 1</a>-->
+<!--            <a class="block md:text-white mr-4" href="/link">Link 2</a>-->
+<!--            <a class="block md:text-white mr-4" href="/link">Link 3</a>-->
+<!--            <a class="block md:text-white mr-4" href="/link">Link 4</a>-->
+<!--          </div>-->
+<!--          <div class="flex text-sm">-->
+<!--            <a class="p-2 ml-2 bg-white text-teal-500 font-semibold leading-none border border-gray-100 rounded hover:border-transparent hover:bg-gray-100" href="/auth/signin">Login</a>-->
+<!--            <a class="p-2 ml-2 bg-teal-500 text-gray-100 font-semibold leading-none border border-teal-600 rounded hover:border-transparent hover:bg-teal-600" href="/auth/signup">Sign up</a>-->
+<!--          </div>-->
+<!--        </div>-->
       </nav>
     </header>
     <main class="md:px-6 px-8 py-12 md:pt-24 pb-24 py-10 text-center text-gray-700 dark:text-gray-200">
